17b7179933ecd83e8118a0c8ab82cfd6573040e7
Kant <my6jay30@gmail.com>
2019-04-05 00:33:35 +0800

    Version 0.1
    測試網頁購買OK

--- app.py
+++ app.py
@@ -1,24 +1,15 @@
 from flask import Flask, render_template
 from flask_restful import Api, Resource
-#from flask_jwt import JWT
 
-#from security import authenticate, identity
 from resources.item import ItemList, ItemBuy
 
 app = Flask(__name__)
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-#app.config['PROPAGATE_EXCEPTIONS'] = True
+
 app.secret_key = 'kant'
 api = Api(app)
 
 
-#@app.before_first_request
-#def create_tables():
-#    db.create_all()
 
-
-#jwt = JWT(app, authenticate, identity)  # /auth
 @app.route('/')
 def home():
     return render_template('index.html')
--- resources/item.py
+++ resources/item.py
@@ -1,53 +1,11 @@
 from flask_restful import Resource
-#from flask_jwt import jwt_required
-#from models.item import ItemModel
+
 from flask import request
 import psycopg2
 from psycopg2 import errorcodes
 from datetime import datetime
 
-#class Item(Resource):
-#    def get(self, name):
-#        item = ItemModel.find_by_name(name)
-#        if item:
-#            return item.json()
-#        return {'message': 'Item not found'}, 404
-#
-#    def post(self, name):
-#        if ItemModel.find_by_name(name):
-#            return {'message': "An item with name '{}' already exists.".format(name)}, 400
-#
-#        data = Item.parser.parse_args()
-#
-#        item = ItemModel(name, **data)
-#
-#        try:
-#            item.save_to_db()
-#        except:
-#            return {"message": "An error occurred inserting the item."}, 500
-#
-#        return item.json(), 201
-#
-#    def delete(self, name):
-#        item = ItemModel.find_by_name(name)
-#        if item:
-#            item.delete_from_db()
-#            return {'message': 'Item deleted.'}
-#        return {'message': 'Item not found.'}, 404
-#
-#    def put(self, name):
-#        data = Item.parser.parse_args()
-#
-#        item = ItemModel.find_by_name(name)
-#
-#        if item:
-#            item.price = data['price']
-#        else:
-#            item = ItemModel(name, **data)
-#
-#        item.save_to_db()
-#
-#        return item.json()
+
 
 
 class ItemList(Resource):
